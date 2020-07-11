ed4ad536dfd2c156c2f292c0dd98dd901bb053b3
Carter Parrett <cparrett94@hotmail.com>
2020-07-11 00:13:27 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,47 @@
 
+def sort_array_asc(array)
+  array.sort
+end
+
+def sort_array_desc(array)
+  array.sort.reverse
+end
+
+def sort_array_char_count(array)
+  array.sort do |a,b|
+    a.length <=> b.length
+  end
+end
+
+def swap_elements(array)
+ array[0], array[1], array[2] = array[0], array[2], array[1]
+end
+
+def reverse_array(array)
+  array.reverse
+end
+
+def kesha_maker(array)
+  dollar_array = []
+  array.each do |a|
+    a[2] = "$"
+  end
+end
+  
+def find_a(array)
+  array.select do |a|
+  a.start_with?('a')
+ end
+end
+  
+def sum_array(array)
+  array.sum
+end
+
+def add_s(array)
+  [0,2,3].each_with_index.collect{|element, index|}
+  
+end
+
+#   #add_s
+#     Add an "s" to each word in the array except for the 2nd element in the array (FAILED - 9)
